b7d86ff7db7b793c882894d2ad401ca3dc120863
max-jellycat <max@jellycat.fr>
2021-02-20 00:50:11 +0100

    fix modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -24,7 +24,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, title, onClose, children }:ModalP
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl h-1/2">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
@@ -40,7 +40,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, title, onClose, children }:ModalP
                   />
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
+                <div className="relative p-6 flex-auto h-96 overflow-y-scroll">
                   {children}
                 </div>
               </div>
